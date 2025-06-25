1ac6ac8174d3f826aee6ad981f11eff422d936a1
Andres Aguilar <aguilarcarboni@gmail.com>
2025-06-24 20:21:03 -0600

    Refactor docker-compose.yml to update agm-trader image to trader-socket:latest and remove frontend configuration files for a cleaner project structure.

--- frontend/src/lib/entities/user.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "next-auth"
-
-export type UserPayload = Omit<User, 'id'> & {
-    password: string
-}
--- frontend/src/lib/types.ts
+++
@@ -1,4 +0,0 @@
-// Array of objects or object of any
-export interface Map {
-    [key: string]: any
-}
